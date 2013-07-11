e52bd60effbb94db89032035594b412c6f16ca31
Xarinor <lukas.walliser@gmail.com>
2013-07-11 14:39:39 +0200

    Updated formerly missleading Documentation

--- classes/FSPurifier.php
+++ classes/FSPurifier.php
@@ -1,15 +1,29 @@
 <?php
-
 /**
  * Contao Open Source CMS
+ * Copyright (C) 2005-2012 Leo Feyer
  *
- * Copyright (C) 2005-2013 Leo Feyer
+ * Formerly known as TYPOlight Open Source CMS.
  *
- * @package Core
- * @link    https://contao.org
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Nothing interactive 2013  <https://www.nothing.ch/>
+ * @author     Lukas Walliser <xari@nothing.ch>
+ * @license http://opensource.org/licenses/lgpl-3.0.html
  */
-
 
 /**
  * Run in a custom namespace, so the class can be replaced
@@ -23,7 +37,6 @@ namespace Contao;
  *
  * @copyright  Nothing interactive 2013  <https://www.nothing.ch/>
  * @author     Lukas Walliser <xari@nothing.ch>
- * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
 class FSPurifier extends \Backend {
 
